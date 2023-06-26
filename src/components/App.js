e5af2c44f1ad2504201e4933c9f7b098ea478481
wael kamira <ramondm4@gmail.com>
2023-06-26 12:08:51 +0300

    Assignment Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+function App() {
+  const [imageUrl, setImageUrl] = useState(null);
+
+  // Fetch the dog image
+  fetch("https://dog.ceo/api/breed/bulldog/english/images/random")
+    .then((response) => response.json())
+    .then((data) => {
+      setImageUrl(data.message);
+    })
+    .catch((error) => {
+      console.error("Error fetching dog image:", error);
+    });
+
+  return (
+    <div>
+      {imageUrl ? (
+        <img src={imageUrl} alt="A Random Dog" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
